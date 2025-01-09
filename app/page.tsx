cf269a8ce6e337609d3e4904ad04bd2c5dcc41b6
Kusala Sameera <kusalasameera,g22@iiits.in>
2025-01-09 12:48:12 +0530

    Added the signup component and changed text in world map and put the component in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import {HeroHighlightDemo} from "../components/LandingPage/HeroSec";
 import {WorldMapDemo} from "../components/LandingPage/WorldMap";
 import {LayoutGridDemo} from "../components/LandingPage/GallerySec";
-
+import {SignupFormDemo} from "../components/LandingPage/Signup"
 export default function Home() {
   return <div className="flex-row items-center justify-center">
     <HeroHighlightDemo />
     <WorldMapDemo />
     <LayoutGridDemo />
+    <SignupFormDemo/>
   </div>;
 }

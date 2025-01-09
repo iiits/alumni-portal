b4ccfb473391d6830e98babafc3a89fa9a97a2ca
Shrishti <125468465+shrishti0308@users.noreply.github.com>
2025-01-09 23:41:38 +0530

    [Feature] update gallery section with descriptive text for houses

--- components/LandingPage/GallerySec.tsx
+++ components/LandingPage/GallerySec.tsx
@@ -10,16 +10,16 @@ export function LayoutGridDemo() {
   );
 }
 
-
 const SkeletonOne = () => {
   return (
     <div>
       <p className="font-bold md:text-4xl text-xl text-white">
-         
+        House in the woods
       </p>
       <p className="font-normal text-base text-white"></p>
       <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-         
+        A serene and tranquil retreat, this house in the woods offers a peaceful
+        escape from the hustle and bustle of city life.
       </p>
     </div>
   );
@@ -29,11 +29,13 @@ const SkeletonTwo = () => {
   return (
     <div>
       <p className="font-bold md:text-4xl text-xl text-white">
-         
+        House above the clouds
       </p>
       <p className="font-normal text-base text-white"></p>
       <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-         
+        Perched high above the world, this house offers breathtaking views and a
+        unique living experience. It&apos;s a place where the sky meets home,
+        and tranquility is a way of life.
       </p>
     </div>
   );
@@ -42,11 +44,12 @@ const SkeletonThree = () => {
   return (
     <div>
       <p className="font-bold md:text-4xl text-xl text-white">
-         
+        Greens all over
       </p>
       <p className="font-normal text-base text-white"></p>
       <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-         
+        A house surrounded by greenery and nature&apos;s beauty. It&apos;s the
+        perfect place to relax, unwind, and enjoy life.
       </p>
     </div>
   );
@@ -55,11 +58,12 @@ const SkeletonFour = () => {
   return (
     <div>
       <p className="font-bold md:text-4xl text-xl text-white">
-         
+        Rivers are serene
       </p>
       <p className="font-normal text-base text-white"></p>
       <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-         
+        A house by the river is a place of peace and tranquility. It&apos;s the
+        perfect place to relax, unwind, and enjoy life.
       </p>
     </div>
   );

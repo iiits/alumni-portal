df44d81d8bfcfe960507ae9585978b065796c91c
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2025-01-10 14:55:57 +0530

    [Refactor] Update font size and max width in HeroHighlightDemo component; reorder imports in hero-highlight.tsx

--- components/LandingPage/HeroSec.tsx
+++ components/LandingPage/HeroSec.tsx
@@ -18,7 +18,7 @@ export function HeroHighlightDemo() {
           duration: 0.5,
           ease: [0.4, 0.0, 0.2, 1],
         }}
-        className="text-4xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+        className="text-4xl px-4 md:text-4xl lg:text-6xl font-bold text-neutral-700 dark:text-white max-w-6xl leading-relaxed lg:leading-snug text-center mx-auto "
       >
         Once a part of us, always a part of us.
         <div>

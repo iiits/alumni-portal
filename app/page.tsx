e313562d4645a91d05f452b55ed305ab9efad36b
Shrishti <125468465+shrishti0308@users.noreply.github.com>
2025-01-10 14:15:53 +0530

    [Refactor] Updated contact email in footer items

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { AnimatedTestimonial } from "@/components/LandingPage/AnimatedTestimonials";
-import { LayoutGridDemo } from "../components/LandingPage/GallerySec";
-import { HeroHighlightDemo } from "../components/LandingPage/HeroSec";
-import { WorldMapDemo } from "../components/LandingPage/WorldMap";
-import {InfiniteMovingCardsDemo} from "../components/LandingPage/companies"
+import { LayoutGridDemo } from "@/components/LandingPage/GallerySec";
+import { HeroHighlightDemo } from "@/components/LandingPage/HeroSec";
+import { WorldMapDemo } from "@/components/LandingPage/WorldMap";
+import {InfiniteMovingCardsDemo} from "@/components/LandingPage/Companies"
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">

bf92ae7fd6599ce1b6b959f18108ea0af620b78d
varshithab05 <117563974+varshithab05@users.noreply.github.com>
2025-02-16 20:51:41 +0530

    removed test code

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -4,13 +4,7 @@ import axios from "axios";
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { name, username, batch, department, collegeEmail, personalEmail, rollNumber, password } = body;
 
-    // if (!name || !username || !batch || !department || !collegeEmail || !personalEmail || !rollNumber || !password) {
-    //   return NextResponse.json({ message: "All fields are required." }, { status: 400 });
-    // }
-
-    // Call the backend API
     const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/register`, body);
 
     return NextResponse.json(response.data, { status: response.status });

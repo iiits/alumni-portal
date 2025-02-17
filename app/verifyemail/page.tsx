375947534185efe24e375f089ec244a44882b235
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2025-02-17 14:21:27 +0530

    [Enhance] Add Suspense for loading state in email verification component

--- app/verifyemail/page.tsx
+++ app/verifyemail/page.tsx
@@ -1,8 +1,12 @@
 import VerifyEmail from "@/components/VerifyEmail/Verifyemail";
+import { Suspense } from "react";
+
 export default function verifyEmail() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <VerifyEmail />
+      <Suspense fallback={<div>Loading...</div>}>
+        <VerifyEmail />
+      </Suspense>
     </div>
   );
 }

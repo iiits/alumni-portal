3092e6c7d51b302d57ab230f99f49ac14d748b93
S-a-m-93 <kusalasameera.g22@iiits.in>
2025-02-17 08:08:09 +0530

    [Add] FAQ page

---
+++ app/faq/page.tsx
@@ -0,0 +1,8 @@
+import { Faq } from "@/components/FaqPage/Faq";
+export default function FaqPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+      <Faq />
+    </div>
+  );
+}
